81a3f9e61c0861777fbc182e4947adf0bf8c50bd
Anastasiia Sinichenkova <anasharik@gmail.com>
2026-03-23 11:16:09 +0500

    Исправлена ошибка по замечанию CheckStyle

--- hw06-jpa/src/main/java/ru/otus/hw/models/Comment.java
+++ hw06-jpa/src/main/java/ru/otus/hw/models/Comment.java
@@ -12,7 +12,6 @@ import jakarta.persistence.NamedAttributeNode;
 import jakarta.persistence.NamedEntityGraph;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
