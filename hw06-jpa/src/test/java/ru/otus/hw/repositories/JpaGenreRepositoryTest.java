75ab5b4731080e0acd6cb4ad7b781fdd47158d48
Anastasiia Sinichenkova <anasharik@gmail.com>
2026-04-02 04:23:29 +0500

    Удален лишний импорт

--- hw06-jpa/src/test/java/ru/otus/hw/repositories/JpaGenreRepositoryTest.java
+++ hw06-jpa/src/test/java/ru/otus/hw/repositories/JpaGenreRepositoryTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
-import org.springframework.util.StringUtils;
 import ru.otus.hw.models.Genre;
 
 import static org.assertj.core.api.Assertions.assertThat;
